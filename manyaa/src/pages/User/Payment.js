f82d5b41e0b33397be534fb723ef287a669de814
Bharat Sharma <21104023bharatsharma@gmail.com>
2023-09-23 22:38:26 +0530

    Update Payment.js

--- manyaa/src/pages/User/Payment.js
+++ manyaa/src/pages/User/Payment.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { styled, } from '@mui/material/styles'; 
+import { React, useState } from 'react';
+import { styled, } from '@mui/material/styles';
 import SideBar from '../../Components/SideBar';
-import {Box,Stack} from '@mui/material';
-import Card from "../../Components/Card";
-import axios from 'axios';
+import { Box, Stack } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -12,57 +19,101 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
 }));
+
+
 const Payment = () => {
 
-  const checkoutHandler = async (amount) => {
-    try {
-      const {data:{key}} = await axios.get(`${process.env.REACT_APP_BACKEND_ADDRESS}/api/v1/getKey`)
-      const {data:{order}} = await axios.post(`${process.env.REACT_APP_BACKEND_ADDRESS}/api/v1/checkout`, {
-        amount
-      });
-      const options = {
-        key: key, 
-        amount: order.amount,
-        currency: "INR",
-        name: "Bharat Sharma",
-        description: "Test Transaction",
-        image: "https://avatars.githubusercontent.com/u/97161064?v=4",
-        order_id: order.id,
-        callback_url: `${process.env.REACT_APP_BACKEND_ADDRESS}/api/v1/paymentVarification`,
-        prefill: {
-            name: "Gaurav Kumar",
-            email: "gaurav.kumar@example.com",
-            contact: "9000090000"
-        },
-        notes: {
-            address: "Razorpay Corporate Office"
-        },
-        theme: {
-            color: "#3399cc"
-        },
-    };
-    const razor = new window.Razorpay(options);
-      razor.open();
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
+
   return (
-    <Box style={{display:'flex'}}>
-    <SideBar/>
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <DrawerHeader />
-     <Box>
-     <Box>
-      <Stack h={"100vh"} direction='row' spacing={2}>
-        <Card amount={5000} img={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"} checkoutHandler={checkoutHandler} />
-        <Card amount={3000} img={"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"} checkoutHandler={checkoutHandler} />
-      </Stack>
+    <Box style={{ display: 'flex' }}>
+      <SideBar />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Box>
+          <Stack spacing={6} >
+            {/* <Stack direction='row' sx={{ Width: '100%' }} spacing={10}>
+                <Box sx={{ Width: '100%' }}>
+                  <FormControl sx={{ Width: '100%' }}>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                      
+                    >
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+
+                <Box sx={{ Width: '100%' }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Stack> */}
+            <Box>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead sx={{backgroundColor:'#004A55'}}>
+                    <TableRow >
+                      <TableCell style={{color:'white',fontWeight:'bold'}}>Invoid ID</TableCell>
+                      <TableCell style={{color:'white',fontWeight:'bold'}}  align="right">Amount</TableCell>
+                      <TableCell style={{color:'white',fontWeight:'bold'}} align="right">Status&nbsp;</TableCell>
+                      <TableCell style={{color:'white',fontWeight:'bold'}} align="right">Date&nbsp;</TableCell>
+                      <TableCell style={{color:'white',fontWeight:'bold'}} align="right">Action&nbsp;</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody sx={{backgroundColor:'#E2EEEE'}}>
+                    {rows.map((row) => (
+                      <TableRow
+                        key={row.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } , borderBottom: '2px solid #A7CDCC',}}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.calories}</TableCell>
+                        <TableCell align="right">{row.fat}</TableCell>
+                        <TableCell align="right">{row.carbs}</TableCell>
+                        <TableCell align="right">{row.protein}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </Stack>
+        </Box>
       </Box>
-     </Box>
     </Box>
-  </Box>
   )
 }
 
